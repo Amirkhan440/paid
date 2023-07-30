4a98aba1d1a15576d7f33ee788c6e0435d1b5fe4
Amirkhan440 <95963441+Amirkhan440@users.noreply.github.com>
2023-07-30 12:50:36 +0500

    Update amir.py

--- amir.py
+++ amir.py
@@ -1,2 +1,9 @@
+import os
+import time
+os.system('git pull')
+print('\033[92mInstalling Missing Moules...')
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+os.system('clear')
+time.sleep(3)
 import bbtox
 bbtox.xd()
